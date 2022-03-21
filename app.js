67978b49241ac6f02758b63ab2223e8f8d7c5b1f
Whitaker <cwhitaker@FVFZMY1CJ1WK.tld>
2022-03-21 10:40:44 -0500

    second commit

---
+++ app.js
@@ -0,0 +1,8 @@
+
+alert("Welcome to 20! This is a competition to see which player gets closer to the number 20. You are going to be playing agains the AI. You will draw numbers 1 to 10. The AI will stop at 16. Pay attention to the screen to see what your total is, and if you need another number, press 'y'.")
+
+alert("Now... it's the AI's turn")
+
+var ai = 0;
+var me = 0;
+
